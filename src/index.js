0499354170e106f5878b5fad4b00cdafc03685b3
Caterina-Panzone <caterina.p@hotmail.com>
2020-09-28 16:53:19 -0300

    adicion de los archivos scss que faltaron de material-kit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "assets/material-kit/scss/material-kit-react.scss?v=1.8.0";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "react-fake-tweet/dist/index.css";
@@ -10,3 +11,4 @@ import "./assets/mdbreact/scss/mdb.scss";
 import "./assets/custom/scss/export.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
