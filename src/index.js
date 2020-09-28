fb1c355932cf063f7b71be89272f2779a93397a6
Caterina-Panzone <caterina.p@hotmail.com>
2020-09-28 20:49:47 -0300

    cambios en los estilos para que funcione el deploy en heroku

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "assets/material-kit/scss/material-kit-react.scss?v=1.8.0";
+import "assets/material-kit/css/material-kit-react.css?v=1.8.0";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "react-fake-tweet/dist/index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./assets/mdbreact/scss/mdb.scss";
-import "./assets/custom/scss/export.scss";
+import "./assets/custom/css/export.css";
 import App from "./App";    
 
 ReactDOM.render(<App />, document.getElementById("root"));
