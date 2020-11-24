9ba5ee0c719828ea444bb0a8cea85260d4ed0cb9
Caterina-Panzone <caterina.p@hotmail.com>
2020-11-24 17:58:48 -0300

    Emotion analyzer sin los datos guardados en el local storage

--- src/components/analyzers/emotionAnalyzer/EmotionAnalyzer.jsx
+++ src/components/analyzers/emotionAnalyzer/EmotionAnalyzer.jsx
@@ -14,39 +14,21 @@ import { NoContentComponent } from "components/shared/noContent/NoContent";
 import { ResponsiveTablePaginator } from "components/shared/paginator/ResponsiveTablePaginator";
 import { Tweet } from "components/shared/tweet/Tweet";
 import { SimilarityAlgorithmsKeys } from "components/similarityAlgorithms/SimilarityAlgorithmsNames";
-import { AuthContext } from "contexts/AuthContext";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import { get } from "utils/api/api.js";
-import { saveSelectedData } from "utils/localStorageManagement/selectedData";
 import { CardSubheader } from "../common/CardSubheader";
 import "./EmotionAnalyzer.scss";
 import { getTweetAndEmotions } from "./getTweetAndEmotions";
 import { SummaryChart } from "./SummaryChart";
 
 export const EmotionAnalyzer = () => {
-  const { selectedData, setSelectedData } = useContext(AuthContext);
   const [hasTweets, setHasTweets] = useState(undefined);
   const [tweetAndEmotions, setTweetAndEmotions] = useState(undefined);
   const [total, setTotal] = useState(0);
   const [page, setPage] = useState(1);
   const [tweetsPerPage, setTweetsPerPage] = useState(6);
   const [count, setCount] = useState(0);
-  const [wasExecuted, setWasExecuted] = useState(false);
-
-  useEffect(() => {
-    if (!wasExecuted && selectedData && selectedData.emotionAnalysis) {
-      get(
-        "/emotionAnalyzer?page=" +
-          1 +
-          "&per_page=" +
-          6 +
-          "&topicTitle=" +
-          selectedData.emotionAnalysis.topicTitle
-      ).then((response) => {
-        setResults(response.data);
-      });
-    }
-  }, [selectedData, wasExecuted]);
+  const [selectedData, setSelectedData] = useState(undefined);
 
   const setResults = (results) => {
     setPage(parseInt(results.page));
@@ -56,74 +38,60 @@ export const EmotionAnalyzer = () => {
     setTotal(parseInt(results.total));
   };
 
-  const getTweets = (page, per_page) => {
+  const getTweets = (page, per_page, data = selectedData) => {
     get(
       "/emotionAnalyzer?page=" +
         page +
         "&per_page=" +
         per_page +
         "&topicTitle=" +
-        selectedData.emotionAnalysis.topicTitle
+        data.topicTitle +
+        "&reportId=" +
+        data.reportId +
+        "&algorithm=" +
+        data.algorithm +
+        "&threshold=" +
+        data.threshold
     ).then((response) => {
       setResults(response.data);
     });
   };
 
   const handleSubmit = (reportId, topicTitle, algorithm, threshold) => {
-    setWasExecuted(true);
     setTweetAndEmotions(undefined);
     const newSelectedData = {
-      ...selectedData,
-      emotionAnalysis: {
-        topicTitle: topicTitle,
-        algorithm: algorithm,
-        threshold: threshold,
-      },
+      topicTitle: topicTitle,
+      reportId: reportId,
+      algorithm: algorithm,
+      threshold: threshold,
     };
-    saveSelectedData(newSelectedData);
     setSelectedData(newSelectedData);
-    get(
-      "/emotionAnalyzer?page=1&per_page=" +
-        tweetsPerPage +
-        "&topicTitle=" +
-        topicTitle +
-        "&reportId=" +
-        reportId +
-        "&algorithm=" +
-        algorithm +
-        "&threshold=" +
-        threshold
-    ).then((response) => {
-      setResults(response.data);
-    });
+    getTweets(1, tweetsPerPage, newSelectedData);
   };
 
   const getSubheader = () => {
     return (
       <CardSubheader
         labels={
-          selectedData.emotionAnalysis.algorithm
+          selectedData.algorithm
             ? [
                 {
                   title: "Tweets",
-                  value: selectedData.emotionAnalysis.topicTitle,
+                  value: selectedData.topicTitle,
                 },
                 {
                   title: "Algoritmo",
-                  value:
-                    SimilarityAlgorithmsKeys[
-                      selectedData.emotionAnalysis.algorithm
-                    ].name,
+                  value: SimilarityAlgorithmsKeys[selectedData.algorithm].name,
                 },
                 {
                   title: "Umbral",
-                  value: selectedData.emotionAnalysis.threshold,
+                  value: selectedData.threshold,
                 },
               ]
             : [
                 {
                   title: "Tweets",
-                  value: selectedData.emotionAnalysis.topicTitle,
+                  value: selectedData.topicTitle,
                 },
               ]
         }
@@ -141,7 +109,7 @@ export const EmotionAnalyzer = () => {
         handleSubmit={handleSubmit}
         setHasTweets={setHasTweets}
       />
-      {selectedData && selectedData.emotionAnalysis ? (
+      {selectedData ? (
         <>
           <Card className="card-row">
             <CardHeader
@@ -150,7 +118,7 @@ export const EmotionAnalyzer = () => {
               className="pdg-btm-0"
             />
             <CardContent className="pdg-top-0">
-              <SummaryChart />
+              <SummaryChart selectedData={selectedData} />
             </CardContent>
           </Card>
           <Card className="card-row">
@@ -163,15 +131,12 @@ export const EmotionAnalyzer = () => {
                     asIcon={true}
                     url={
                       "/emotionAnalyzer/download?topicTitle=" +
-                      selectedData.emotionAnalysis.topicTitle
+                      selectedData.topicTitle
                     }
                     disableDownload={
                       !tweetAndEmotions || tweetAndEmotions.length === 0
                     }
-                    filename={
-                      selectedData.emotionAnalysis.topicTitle +
-                      "-emotion-analysis"
-                    }
+                    filename={selectedData.topicTitle + "-emotion-analysis"}
                   />
                 </>
               }
@@ -237,9 +202,9 @@ export const EmotionAnalyzer = () => {
             </CardContent>
           </Card>
         </>
-      ) : hasTweets && (!selectedData || !selectedData.emotionAnalysis) ? (
-        <Paper style={{ padding: 5, marginTop: 15 }}>
-          <Box className="no_content_box">
+      ) : hasTweets && !selectedData ? (
+        <Paper className="card-row" style={{ padding: 5 }}>
+          <Box className="no-content-box">
             {NoContentComponent(
               "Aún no realizaste un análisis de emociones",
               "¡Obtené las emociones del conjunto de tweets que desees!",
--- src/components/analyzers/emotionAnalyzer/SummaryChart.jsx
+++ src/components/analyzers/emotionAnalyzer/SummaryChart.jsx
@@ -1,31 +1,27 @@
 import Skeleton from "@material-ui/lab/Skeleton";
 import { EmotionChart } from "components/shared/tweet/EmotionChart";
-import { AuthContext } from "contexts/AuthContext";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { get } from "utils/api/api";
 import { getEmotions } from "./getTweetAndEmotions";
 import "./SummaryChart.scss";
 
-export const SummaryChart = () => {
-  const { selectedData } = useContext(AuthContext);
+export const SummaryChart = ({ selectedData, ...props }) => {
   const [topicEmotions, setTopicEmotions] = useState(undefined);
 
   useEffect(() => {
-    if (selectedData.emotionAnalysis) {
-      setTopicEmotions(undefined);
-      get(
-        "/emotionAnalyzer/topic?topicTitle=" +
-          selectedData.emotionAnalysis.topicTitle +
-          "&reportId=" +
-          selectedData.report?.id +
-          "&algorithm=" +
-          selectedData.emotionAnalysis.algorithm +
-          "&threshold=" +
-          selectedData.emotionAnalysis.threshold
-      ).then((response) => {
-        setTopicEmotions(getEmotions(response.data));
-      });
-    }
+    setTopicEmotions(undefined);
+    get(
+      "/emotionAnalyzer/topic?topicTitle=" +
+        selectedData.topicTitle +
+        "&reportId=" +
+        selectedData.reportId +
+        "&algorithm=" +
+        selectedData.algorithm +
+        "&threshold=" +
+        selectedData.threshold
+    ).then((response) => {
+      setTopicEmotions(getEmotions(response.data));
+    });
   }, [selectedData]);
 
   return topicEmotions ? (
